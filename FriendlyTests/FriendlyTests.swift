91179c4bd876b4197f82d7e8f2c2345617ff5bfc
Nathan Mercier <nathanmercier@orange.fr>
2025-02-07 15:06:25 +0100

    Initial Commit

---
+++ FriendlyTests/FriendlyTests.swift
@@ -0,0 +1,16 @@
+//
+//  FriendlyTests.swift
+//  FriendlyTests
+//
+//  Created by Nathan Mercier on 07/02/2025.
+//
+
+import Testing
+
+struct FriendlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
